7fd8793cda3aaa9188b0caa66d94563514b61997
ArturFonsecaDev <sgsartur@hotmail.com>
2024-12-30 18:22:41 -0400

    Helper function added to avoid repeating code

--- src/requests/KanbanRequests.js
+++ src/requests/KanbanRequests.js
@@ -1,13 +1,17 @@
 import { fetchUrls } from './auth.js';
 
+function getHeaders(token){
+  return {
+    'Authorization': `Bearer ${token}`,
+    'Content-Type': 'application/json',
+  }
+}
+
 export async function listKanbans(token, refreshToken){
   const url = 'http://127.0.0.1:8000/api/todo';
   const payload = {
     method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    }
+    headers: getHeaders()
   };
 
   const data = await fetchUrls(url, payload, refreshToken);
@@ -17,4 +21,23 @@ export async function listKanbans(token, refreshToken){
     kanbans: response,
     newAccess: newToken
   }
+}
+
+export async function createKanban(name, id, token, refreshToken){
+  const url = 'http://127.0.0.1:8000/api/todo';
+  const payload = {
+
+  }
+  const options = {
+    method: 'POST',
+    headers: getHeaders(),
+    body: {
+      name: name,
+      user: id
+    }
+  };
+
+  const data = await fetchUrls(url, options, )
+
+
 }
